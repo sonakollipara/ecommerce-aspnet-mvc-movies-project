1a26074fc24e2ca62799ca4a2af3690bda0c086d
kollipara <sonakollipara@gmail.com>
2023-07-01 20:38:49 +0530

    ecommerce first commit

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+	public class ProducersController : Controller
+	{
+		private readonly AppDbContext _Context;
+        public ProducersController(AppDbContext Context)
+        {
+				_Context = Context;
+        }
+        public async Task<IActionResult> Index()
+		{
+			var allProducers =await _Context.Producers.ToListAsync();
+			return View(allProducers);
+		}
+	}
+}
---
+++ Data/enum/MovieCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Data.@enum
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentory,
+        Cartoon,
+        Horror,
+        Documentary
+    }
+}
